82483efadb383fa41d838677ba02d011225b342a
Michael Burleson <mike.burleson@me.com>
2023-12-06 15:41:00 -0500

    Tests are passing on part one

---
+++ day 01/calibrator.py
@@ -0,0 +1,15 @@
+import re
+
+print('Opening input file...')
+with open('test.txt') as file:
+    lines = file.readlines()
+
+totalSum = 0
+
+for line in lines:
+    numbers = re.findall(r'\d+', line)
+    firstNum = int(numbers[0])
+    lastNum = int(numbers[-1])
+    totalSum += (firstNum * 10) + lastNum
+
+print(totalSum)
