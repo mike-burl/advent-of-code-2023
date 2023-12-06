c5469b16eafea05667568c92569058f40859820b
Michael Burleson <mike.burleson@me.com>
2023-12-06 15:45:25 -0500

    Fixed bug where numbers extracted from line could be multiple digits

--- day 01/calibrator.py
+++ day 01/calibrator.py
@@ -7,7 +7,7 @@ with open('test.txt') as file:
 totalSum = 0
 
 for line in lines:
-    numbers = re.findall(r'\d+', line)
+    numbers = re.findall(r'\d', line)
     firstNum = int(numbers[0])
     lastNum = int(numbers[-1])
     totalSum += (firstNum * 10) + lastNum
